d1555f9dbc0c365b46e3ae894674f6b13de5b761
Emily Tan <emitan0125@gmail.com>
2019-03-11 15:36:49 -0400

    fixed formatting

--- uwu.py
+++ uwu.py
@@ -3,7 +3,7 @@ import os, sys
 from wallaby import *
 
 def main():
-  #FUNCTION VARIABLES
+  # -- function variables -- #
   LM = 0
   RM = 1
   TM = 2 #turn motor
@@ -13,20 +13,9 @@ def main():
   PS = -100
   right_angle = 250
   line_follow_threshold = 50
-  
-  #PROGRAM START
-  #move towards firemen
- # drive_forwards_fast(100) #adjust this so the wallaby gets to the point where it can start turning the motor
- # off(8) #turns motors off for 5 seconds *i think*
 
-  #move firemen out of pole
- #  for f in range (5): #for each of the 5 firemen in the pole
- #  	print "first loop"
- #  	for i in range (0,6): #turn motor then stop 6 times to knock each block out (play with the number of times the effector turns/stops and the power)
-	# 	print "second loop"
-	# 	motor(TM,50)
-	# 	aosleep(3000)
-	# motor(TM,80) #swing effector back around for the next block
+  # -- program start -- #
+  
   i = 0
   while i<20000:
     i+=1
@@ -34,7 +23,7 @@ def main():
   aosleep(10000)
 
 
-#FUNCTION DEFINITIONS 
+# -- function definitions -- #
 def drive_forwards_fast(time):
   i = 0
   while i <= time:
@@ -52,25 +41,25 @@ def drive_backwards_fast(time):
   while i <= time:
     motor(LM,-HS)
     motor(RM,-HS)
-    
+
 def drive_backwards_slow(time):
   i = 0
   while i <= time:
     motor(LM,-NS)
     motor(RM,-NS)
-    
+
 def pivot_left(time):
   i = 0
   while i <= time:
     motor(LM,PS)
     motor(RM,HS)
-    
+
 def pivot_right(time):
   i = 0
   while i <= time:
     motor(LM,HS)
     motor(RM,PS)
-    
+
 def line_followL(threshold,time):
   i = 0
   while i <= time:
@@ -82,7 +71,7 @@ def line_followL(threshold,time):
       for x in range (0,5):
         motor(RM,100)
         motor(LM,65)
-        
+
 def line_followR(threshold,time):
   i = 0
   while i <= time:
@@ -94,11 +83,11 @@ def line_followR(threshold,time):
       for x in range (0,5):
         motor(LM,100)
         motor(RM,65)
-        
+
 def aosleep(ms):
   ao()
   msleep(ms)
-        
+
 if __name__== "__main__":
     sys.stdout = os.fdopen(sys.stdout.fileno(),"w",0)
     main();
