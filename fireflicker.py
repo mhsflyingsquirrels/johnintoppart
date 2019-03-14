2cf9c24c6864def30613c02a1cc9905fb3524ad4
Emily Tan <emitan0125@gmail.com>
2019-03-14 16:28:54 -0400

    stuff works now

--- fireflicker.py
+++ fireflicker.py
@@ -2,67 +2,50 @@
 import os, sys
 from wallaby import *
 
-def main():
-  # -- function variables -- #
-  LM = 0
-  RM = 1
-  TM = 2 #turn motor
-  HS = 100
-  NS = 80
-  TS = -10
-  PS = -100
-  right_angle = 250
-  line_follow_threshold = 50
-
-  # -- program start -- #
-  
-  i = 0
-  while i<20000:
-    i+=1
-    motor(TM, 100)
-  aosleep(10000)
-
-
+# -- function variables -- #
+LM = 0
+RM = 1
+TM = 2 
+HS = 100
+NS = 80
+TS = -10
+PS = -100
+right_angle = 250
+line_follow_threshold = 50
+    
 # -- function definitions -- #
 def drive_forwards_fast(time):
-  i = 0
-  while i <= time:
+  for i in range(0,time):
     motor(LM, HS)
     motor(RM,HS)
 
 def drive_forwards_slow(time):
-  i = 0
-  while i <= time:
+  for i in range(0,time):
     motor(LM,NS)
     motor(RM,NS)
 
 def drive_backwards_fast(time):
-  i = 0
-  while i <= time:
+  for i in range(0,time):
     motor(LM,-HS)
     motor(RM,-HS)
 
 def drive_backwards_slow(time):
-  i = 0
-  while i <= time:
+  for i in range(0,time):
     motor(LM,-NS)
     motor(RM,-NS)
 
 def pivot_left(time):
-  i = 0
-  while i <= time:
+  for i in range(9,time):
     motor(LM,PS)
     motor(RM,HS)
 
 def pivot_right(time):
-  i = 0
-  while i <= time:
+  for i in range(0,time):
     motor(LM,HS)
     motor(RM,PS)
 
 def line_followL(threshold,time):
-  i = 0
-  while i <= time:
+  for i in range(0,time):
     if analog(0) <= threshold:
       for x in range (0,5):
         motor(LM,100)
@@ -73,8 +56,7 @@ def line_followL(threshold,time):
         motor(LM,65)
 
 def line_followR(threshold,time):
-  i = 0
-  while i <= time:
+  for i in range(0,time):
     if analog(0) <= threshold:
       for x in range (0,5):
         motor(RM,100)
@@ -83,10 +65,45 @@ def line_followR(threshold,time):
       for x in range (0,5):
         motor(LM,100)
         motor(RM,65)
-
+ 
 def aosleep(ms):
   ao()
   msleep(ms)
+      
+def main():
+  # -- program start -- #
+
+
+  #drive towards pole
+  off(TM)
+  drive_forwards_fast(2400) 
+  off(RM)
+  off(LM)
+  msleep(200)
+      
+  #turn adjustment
+  pivot_right(30)
+  off(RM)
+  off(LM)
+  msleep(200)
+  
+  #scooch forwards a little
+  drive_forwards_fast(5)
+  off(RM)
+  off(LM)
+  msleep(200)
+  
+  #get firefighters out of pole
+  i = 0
+  while i<20000:
+    i+=1
+    motor(TM, 100)
+  
+        
+        
+  # -- program end -- #
+
+
 
 if __name__== "__main__":
     sys.stdout = os.fdopen(sys.stdout.fileno(),"w",0)
